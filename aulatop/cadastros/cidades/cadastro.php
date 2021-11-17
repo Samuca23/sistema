b155c7c46fa007154e66b007d6915eee51e48c56
Samuel Chiodini <samuelchiodini@hotmail.com>
2021-11-17 20:14:19 -0300

    select

--- aulatop/cadastros/cidades/cadastro.php
+++ aulatop/cadastros/cidades/cadastro.php
@@ -1,4 +1,8 @@
 <?php
+$stmt = $conn->prepare('SELECT * FROM estados');
+$stmt->execute();
+$result = $stmt->fetchAll();
+
 if (isset($_POST['gravar'])) {
     try {
         $stmt = $conn->prepare(
@@ -12,14 +16,26 @@ if (isset($_POST['gravar'])) {
     }
 }
 ?>
+
 <form method="post">
     <div class="form-group">
         <label for="nome">Código</label>
         <input type="text" class="form-control" name="codigo" id="codigo" placeholder="Código">
         <label for="nome">Nome</label>
         <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome">
-        <label for="nome">Estado</label>
-        <input type="number" class="form-control" name="estado" id="estado" placeholder="Estado">
+    </div>
+    <div class="form-group">
+        <label for="estado">Estado</label>
+        <select class="form-control" name="estado" id="estado">
+            <option value="">** Selecione **</option>
+            <?php
+                foreach ($result as $l) {
+                    ?>
+                        <option selected value="<?=$l['id']?>"><?=$l['sigla']?> - <?=$l['nome']?></option>
+                    <?php
+                }
+            ?>
+        </select>
     </div>
     <input type="submit" name="gravar" value="Gravar">
 </form>
